136cb28644b30652800b3ad21430663f2cae31f5
Noah Theodore Prisament <nprisament@gmail.com>
2020-12-27 03:53:14 -0500

    update newsletter and officers

--- alumni.php
+++ alumni.php
@@ -110,6 +110,9 @@
             Old Alumni Newsletters: <br />
             <ul>
               <li>
+                <a href="newsletter/spring2020.pdf">Spring 2020</a>
+              </li>
+              <li>
                 <a href="newsletter/fall2019.pdf">Fall 2019</a>
               </li>
               <li>
